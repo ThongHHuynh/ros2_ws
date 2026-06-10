933c47131a454b51d377ce2e54ab3be3495ad523
tom <thonghuynh.0203@gmail.com>
2026-06-09 21:37:14 -0400

    Fix lidar frame

--- src/my_robot_bringup/launch/navigation.launch.py
+++ src/my_robot_bringup/launch/navigation.launch.py
@@ -25,7 +25,8 @@ def generate_launch_description():
     gazebo_config_path = os.path.join(robot_bringup_path, 'config', 'gazebo_bridge.yaml')
     nav2_params_path = os.path.join(robot_bringup_path, 'config', 'nav2_params.yaml')
     world_path = os.path.join(robot_description_path, 'worlds', 'maze.sdf')
-    nav_map_path = '/home/tom/maps/simple_maze.yaml'
+    #nav_map_path = '/home/tom/maps/simple_maze.yaml'
+    nav_map_path = '/ros2_ws/maps/my_map.yaml'
 
     robot_description = ParameterValue(Command(['xacro ', urdf_path,' ',
                                                 'use_mock_hardware:=', 'true' if use_mock else 'false', ' ',
